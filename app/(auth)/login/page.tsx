e217f742ba0217c5c7fcdab0a577bc5c72d9d185
Gokul <gokulchandan24@gmail.com>
2025-07-25 02:25:24 +0530

    changes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,21 @@
 import LoginForm from "@/components/auth/login-form";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <LoginForm />
+    <div className="flex flex-col min-h-screen md:flex-row">
+      <div className="relative h-[50vh] w-full md:h-screen md:w-1/2">
+        <Image
+          src="/woman.jpg"
+          alt="Form Background"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      <div className="flex flex-col gap-8 items-center justify-center w-full md:w-1/2 p-4">
+        <LoginForm />
+      </div>
     </div>
   );
 };
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,10 +1,21 @@
 import MultiStepForm from "@/components/auth/multi-step-form/multi-step-form";
+import Image from "next/image";
 
 const page = () => {
   return (
-    <div className="flex min-h-screen">
-      <div className="sticky top-0 h-screen w-1/3 bg-blue-400 bg-formImage bg-cover"></div>
-      <MultiStepForm />
+    <div className="flex flex-col min-h-screen md:flex-row">
+      <div className="relative h-[50vh] w-full md:h-screen md:w-1/3">
+        <Image
+          src="/wind.jpg"
+          alt="Form Background"
+          fill
+          className="object-cover"
+          priority
+        />
+      </div>
+      <div className="flex flex-col gap-8 items-center justify-center w-full md:w-2/3 p-4">
+        <MultiStepForm />
+      </div>
     </div>
   );
 };
